192a14e0faf0fc2ec4b53a60c5f32241ec32592b
Yuma Morimoto <kanten520212@gmail.com>
2024-04-06 02:07:44 +0900

    Create app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width,initial-scale=1' },
+  ],
+});
+
+useSeoMeta({
+  title: 'IPUT ONE',
+  ogTitle: 'IPUT ONE',
+  description: 'IPUT ONE 国際工科専門職大学サークルサイト',
+  ogDescription: 'IPUT ONE 国際工科専門職大学サークルサイト',
+  ogImage: '',
+  twitterCard: 'summary_large_image',
+});
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
