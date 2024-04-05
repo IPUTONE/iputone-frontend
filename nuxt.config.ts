461ad349314ae87f6a1546e04789e7980e01873a
Yuma Morimoto <kanten520212@gmail.com>
2024-04-06 01:34:42 +0900

    add & move files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  ssr: false,
+  srcDir: 'src/',
+  devServer: {
+    port: 3000,
+  },
+  gtag: {
+    id: '',
+  },
+  modules: ['nuxt-gtag'],
+});
