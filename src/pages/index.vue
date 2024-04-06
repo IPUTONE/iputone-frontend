d9efb6b5fd0aa8c658fc051074075e2bf5a11b2c
Yuma Morimoto <kanten520212@gmail.com>
2024-04-06 14:05:24 +0900

    update pages

---
+++ src/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const baseUrl = 'https://iput-one.com';
+const endpoint = baseUrl + '/wp-json/wp/v2/posts/1';
+const data = await useFetch(endpoint);
+console.log(data);
+</script>
+
+<template></template>
