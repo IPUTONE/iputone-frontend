9450ee482b5ffeaf58fa56b773ccf111852bb85e
Yuma Morimoto <kanten520212@gmail.com>
2024-06-21 13:08:25 +0900

    remove all

--- src/pages/index.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-const baseUrl = 'https://iput-one.com';
-const endpoint = baseUrl + '/wp-json/wp/v2/posts/1';
-const data = await useFetch(endpoint);
-console.log(data);
-</script>
-
-<template></template>
